19a3c64845209cfb670267484a81b5d3774ea82c
Atanu Sarkar <34891206+mratanusarkar@users.noreply.github.com>
2023-09-28 08:37:18 +0000

    add: basic api usage example

---
+++ projects/django/01-web-apis/01-requests/02-requests-api.py
@@ -0,0 +1,29 @@
+"""Explore the use of REST API HTTP Requests with https://exchangeratesapi.io/"""
+
+import requests
+
+def main():
+    """main method to try out exchange-rates-api endpoints"""
+
+    BASE_URL = "https://api.exchangeratesapi.io/latest"
+
+    body = {
+        "base": "USD",
+        "symbols": "GBP"
+    }
+
+    # requests methods can be: GET, POST, PUT, DELETE, etc...
+    res = requests.get(BASE_URL, params=body, timeout=10)
+    print("status code:", res.status_code)
+    print("content type:", res.headers['Content-Type'])
+
+    if res.status_code == 200:
+        data = res.json()
+        print("res json data:", data)
+    else:
+        print("error! no data")
+        raise Exception("There was an error!")
+
+
+if __name__ == "__main__":
+    main()
