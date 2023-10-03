cade6ceb50cc0c0d707a71481f6eb0d5d38eaf50
Atanu Sarkar <34891206+mratanusarkar@users.noreply.github.com>
2023-10-03 13:24:18 +0000

    add: views for gadget list and details

--- projects/django/01-web-apis/02-django/techmart/gadgets/views.py
+++ projects/django/01-web-apis/02-django/techmart/gadgets/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+"""View for Gadgets and Brands"""
 
-# Create your views here.
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+from .models import Gadget, Brand
+
+
+class GadgetDetailView(DetailView):
+    """Gadget Detail View extending django DetailView and linking html template"""
+
+    model = Gadget
+    template_name = "gadgets/gadget_details.html"
+
+
+class GadgetListView(ListView):
+    """Gadget List View extending django ListView and linking html template"""
+
+    model = Gadget
+    template_name = "gadgets/gadget_list.html"
