632334abe8199d8c7ba7c29bb3fea0d7d43dfba6
Atanu Sarkar <34891206+mratanusarkar@users.noreply.github.com>
2023-10-03 04:55:06 +0000

    add: models brands and gadgets

--- projects/django/01-web-apis/02-django/techmart/gadgets/models.py
+++ projects/django/01-web-apis/02-django/techmart/gadgets/models.py
@@ -1,3 +1,25 @@
+"""Model File containing the definitions for Brands and Gadgets"""
+
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    """Defines the Brand or the Manufacturer of Gadgets"""
+    name = models.CharField(max_length=120)
+    location = models.CharField(max_length=120)
+    description = models.TextField(max_length=800, blank=True, null=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return f"""{self.name}"""
+
+
+class Gadget(models.Model):
+    """Defines the Product or the Gadget and it's details"""
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name="gadgets")
+    name = models.CharField(max_length=120)
+    description = models.TextField(max_length=800, blank=True, null=True)
+    photo = models.ImageField(blank=True, null=True)
+    price = models.FloatField()
+
+    def __str__(self) -> str:
+        return f"""{self.name}"""
