127205db59a868dfe614cb4ef31889e05668095c
Atanu Sarkar <34891206+mratanusarkar@users.noreply.github.com>
2023-10-03 13:26:50 +0000

    add: the models to to django admin

--- projects/django/01-web-apis/02-django/techmart/gadgets/admin.py
+++ projects/django/01-web-apis/02-django/techmart/gadgets/admin.py
@@ -1,3 +1,8 @@
+"""register the models to django admin"""
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Gadget, Brand
+
+admin.site.register(Gadget)
+admin.site.register(Brand)
